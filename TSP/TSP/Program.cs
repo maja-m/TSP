1684f3bfb225352f5e892f0bcfd53ba2986375b6
Maja Marchewka <marchewka.maja@gmail.com>
2018-05-15 20:22:54 +0200

    setup

---
+++ TSP/TSP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
